0213723a97f7ab840de13c2c5bcac7afa25e0a6d
Roland Villemoes <rv@alpha-solutions.us>
2025-07-02 22:16:18 -0400

    More logging

--- Controller/Router.php
+++ Controller/Router.php
@@ -133,8 +133,9 @@ class Router implements RouterInterface
             }
 
             $data = $this->serializer->unserialize($data);
+            $this->logger->debug('MediaLounge\Storyblok\Controller\Router::match()::UNserialize::$data=' . json_encode($data));
 
-            $this->logger->debug('MediaLounge\Storyblok\Controller\Router::match()::!empty($data->story)=' . !empty($data->story));
+            $this->logger->debug('MediaLounge\Storyblok\Controller\Router::match()::$data->story=' . json_encode($data->story));
 
 
             if (!empty($data->story)) {
